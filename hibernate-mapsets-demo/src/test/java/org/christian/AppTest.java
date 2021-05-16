159fa9180b908bc7099c51a9f83e8411dbafc080
Christian <christian@sanglech.com>
2021-05-16 21:45:11 +0900

    Initial commit

---
+++ hibernate-mapsets-demo/src/test/java/org/christian/AppTest.java
@@ -0,0 +1,20 @@
+package org.christian;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
